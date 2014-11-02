e829657eca9b4467530170fb7f69034e1f6ee187
marcossilva <marcossilva_604@yahoo.com.br>
2014-11-02 18:26:28 -0200

    Crete Initial Commands

--- EnsaioRetentivo/src/interpreter/Interpreter.java
+++ EnsaioRetentivo/src/interpreter/Interpreter.java
@@ -7,7 +7,11 @@ package interpreter;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -22,18 +26,21 @@ public abstract class Interpreter implements Interprete {
     private JSONArray commands;
     private File json;
     private Scanner in;
-    /**     * @TODO melhor maneira de deixar mensagem de erro genérica?     */
+    /**
+     * * @TODO melhor maneira de deixar mensagem de erro genérica?
+     */
     private String errorMsg;
 
     protected Interpreter(String fileDir, String errorMsg) {
         this.errorMsg = errorMsg;
         //Lê os comandos pré-definidos de um arquivo JSON
-        json = new File(fileDir);
+        json = new File(System.getProperty("user.dir") + fileDir);
         if (!json.exists()) {
             //entrar em modo de debug e inserir comandos do jogo no arquivo
             initializeJSON();
         } else {
             try {
+                System.out.println("Arquivo de comandos encontrado! Lendo entradas!");
                 in = new Scanner(json);
                 //Ler o arquivo 
                 StringBuilder arq = new StringBuilder();
@@ -41,10 +48,12 @@ public abstract class Interpreter implements Interprete {
                     arq.append(in.nextLine());
                     arq.append("\n");
                 }
+                //pq colocar um JSONObject se o arquivo de comandos é certamente um único array?
                 g = new JSONObject(arq.toString());
-                commands = g.getJSONArray("command");
+                commands = g.getJSONArray("commands");
             } catch (FileNotFoundException ex) {
                 //Nunca acontece pois o arquivo é criado com certeza nas linhas acima
+                System.out.println("Arquivo não encontrado :(");
             }
         }
     }
@@ -52,32 +61,64 @@ public abstract class Interpreter implements Interprete {
     private void initializeJSON() {
         //DEBUG MODE
         //Acessível apenas para programadores inserirem comandos no jogo.
-        
+        System.out.println("Entrar no modo debug!");
+        try {
+            json.createNewFile();
+            g = new JSONObject();
+            commands = new JSONArray();
+            commands.put(new JSONObject().put("ver", "observar"));
+            commands.put(new JSONObject().put("observar", "observar"));
+            commands.put(new JSONObject().put("descrever", "observar"));
+            commands.put(new JSONObject().put("analisar", "observar"));
+            commands.put(new JSONObject().put("subir", "movimento"));
+            commands.put(new JSONObject().put("descer", "movimento"));
+            commands.put(new JSONObject().put("ir", "movimento"));
+            commands.put(new JSONObject().put("andar", "movimento"));
+            commands.put(new JSONObject().put("inventorio", "inventorio"));
+            commands.put(new JSONObject().put("inventório", "inventorio"));
+            commands.put(new JSONObject().put("usar", "inventorio"));
+            commands.put(new JSONObject().put("descartar", "inventorio"));
+            g.put("commands", commands);
+            FileWriter writer = new FileWriter(json);
+            writer.write(g.toString());
+            System.out.println(g.toString());
+        } catch (IOException ex) {
+            System.err.println("Can't write to directory.");
+        }
     }
 
     @Override
     public String interpretar(String comando) {
+        if (commands == null) {
+            try {
+                throw new Exception("Inicialize o arquivo de comandos pfvr, obg.");
+            } catch (Exception ex) {
+                Logger.getLogger(Interpreter.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
         double hashInput = comando.hashCode();
         double hashRead = 0;
         double closestHashRead = 0;
-        
+
         int commandIndex = 0;
-        
+
         String retorno = "";
         for (int i = 0; i < commands.length(); i++) {
             hashRead = commands.get(i).toString().hashCode();
-            if (hashRead == hashInput){
-                //execute this command
-                commandIndex = -1;
+            commandIndex = i;
+            if (hashRead == hashInput) {
+                //sai do loop
                 break;
             }
             //Procura o comando mais próximo ao digitado pelo usuário pelo hash do String digitado
-            if (Math.min(closestHashRead, Math.abs(hashRead - hashInput)) != closestHashRead){
-                commandIndex = i;
+            if (Math.min(closestHashRead, Math.abs(hashRead - hashInput)) != closestHashRead) {
                 closestHashRead = Math.min(closestHashRead, Math.abs(hashRead - hashInput));
             }
         }
-        if (commandIndex >= 0){
+        if (closestHashRead == 0) { //O comando achado é o comando digitado
+            System.out.println("O comando digitado foi " + commands.get(commandIndex).toString());
+        }
+        if (commandIndex < 0) {
             //Comando não executado
             /**
              * @TODO Adicionar respostas inusitadas
