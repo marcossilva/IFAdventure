7b7cd06d2ebba42bafb829cb1d2c2093205423d9
marcos <marcos@marcos-K84L>
2014-11-27 00:15:28 -0200

    Itens

---
+++ EnsaioRetentivo/src/item/Item.java
@@ -0,0 +1,15 @@
+package item;
+
+/**
+ *
+ * @author marcos
+ * @time 2:32:23 PM
+ */
+public class Item {
+    private int weight;
+
+    public int getWeight() {
+        return this.weight;
+    }
+    
+}
